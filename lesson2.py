574fe67cd767a0117cdfcee5b02a8abcba7a7124
iamsudipta <iamsudipta142@gmail.com>
2026-08-06 11:24:41 +0530

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,22 @@
+software = "Certification Manager"
+developer = "Sayan Dam"
+company = "MS Certification Services Private Limited"
+address = "62B , Maharaja tagore road , kolkata - 700031"
+print("software :"+ software)
+print("developer" + developer)
+print("company" + company)
+print("address" + address)
+office = "office 7"
+place = "kolkata-700031"
+pincode = "700031"
+done_by = "We"
+submitted_reviewed_by = "Technical Reviewer"
+approved_by = "Managing director S. Dutta"
+certificate_processed_by = "kanu karmakar"
+print("office = " + office)
+print("place = " + place)
+print("pincode = " + pincode)
+print("done by = " + done_by)
+print("submitted & reviewed by = " + submitted_reviewed_by)
+print("approved by = " + approved_by)
+print("certificate processed by = " + certificate_processed_by)
