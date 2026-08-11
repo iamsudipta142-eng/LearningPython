c7bb5aa18d7b58664ad94e82ff2d60b24047c25e
Sudipta Das <iamsudipta142@gmail.com>
2026-08-11 17:43:13 +0530

    message

---
+++ exam4.py
@@ -0,0 +1,17 @@
+auditors = ["auditor 1: sudipta das" , "auditor 2: a roy" , "auditor 3: kanu karmakar" , "auditor 4: Anirban hens"]
+for auditor in auditors:
+    print(auditor)
+auditors = ["safety auditor - Avishek" , 
+            "Safety Expert - Manob" , 
+            "Auditor - S. Das"]
+for auditor in auditors:
+    print(auditor)
+standards = ["iso 9001" ,
+             "iso 14001" ,
+             "iso 45001" ,
+             "iso 50001" ,
+             "iso 27001"]
+count = 1
+for standard in standards:
+    print(str(count) + "." + standard)
+    count = count + 1
